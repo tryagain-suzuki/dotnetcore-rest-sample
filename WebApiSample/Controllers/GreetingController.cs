d87bdeb26c138766a3ea5fa19a3dce66ba9f1a86
tryagain-suzuki <tryagain.suzuki.t@gmail.com>
2020-04-26 13:41:39 +0900

    Add Greeitng.

---
+++ WebApiSample/Controllers/GreetingController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiSample.Services;
+
+namespace WebApiSample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GreetingController : ControllerBase
+    {
+
+        [HttpGet("{name}")]
+        public ActionResult Get(string name)
+            => Ok(new GreetingService().CreateGreeting(name));
+
+    }
+}
---
+++ WebApiSample/Services/GreetingService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiSample.Services
+{
+    public class GreetingService
+    {
+
+        public string CreateGreeting(string name) 
+            => $@"Hello, {name}!!";
+        
+
+    }
+}
